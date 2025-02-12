a2b690452933703d7bd4b9acefe35d196ac8882b
Alejandro de Jesús Escareño Flores <alejandroef04@gmail.com>
2025-02-12 12:48:52 -0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,102 @@
+
+// Variables
+const carrito = document.querySelector('#carrito');
+const listaTeclados = document.querySelector('#lista-teclados');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+let articulosCarrito = [];
+
+cargarEventListeners();
+
+function cargarEventListeners() {
+	// Dispara cuando se presiona "Agregar Carrito"
+	listaTeclados.addEventListener('click', agregarTeclado);
+
+	carrito.addEventListener('click', eliminarTeclado);
+
+	// Al Vaciar el carrito
+	vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+}
+
+
+function agregarTeclado(e) {
+	e.preventDefault();
+	// Delegation para agregar-carrito
+	if (e.target.classList.contains('agregar-item')) {
+		const teclado = e.target.parentElement.parentElement;
+		leerDatosTeclado(teclado);
+	}
+}
+
+
+function leerDatosTeclado(teclado) {
+	const infTeclado = {
+		imagen: teclado.querySelector('img').src,
+		titulo: teclado.querySelector('h4').textContent,
+		precio: teclado.querySelector('.precio span').textContent,
+		id: teclado.querySelector('a').getAttribute('data-id'),
+		cantidad: 1
+	};
+
+
+	if (articulosCarrito.some(teclado => teclado.id === infTeclado.id)) {
+		const teclados = articulosCarrito.map(teclado => {
+			if (teclado.id === infTeclado.id) {
+				teclado.cantidad++;
+				return teclado;
+			} else {
+				return teclado;
+			}
+		});
+		articulosCarrito = [...teclados];
+	} else {
+		articulosCarrito = [...articulosCarrito, infTeclado];
+	}
+
+	carritoHTML();
+}
+
+
+function eliminarTeclado(e) {
+	e.preventDefault();
+	if (e.target.classList.contains('borrar-teclado')) {
+		// e.target.parentElement.parentElement.remove();
+		const tecId = e.target.getAttribute('data-id');
+
+		// Eliminar del arreglo del carrito
+		articulosCarrito = articulosCarrito.filter(teclado => teclado.id !== tecId);
+
+		carritoHTML();
+	}
+}
+
+
+function carritoHTML() {
+
+	vaciarCarrito();
+
+	articulosCarrito.forEach(teclado => {
+		const row = document.createElement('tr');
+		row.innerHTML = `
+			  <td>  
+				   <img src="${teclado.imagen}" width=100>
+			  </td>
+			  <td>${teclado.titulo}</td>
+			  <td>${teclado.precio}</td>
+			  <td>${teclado.cantidad} </td>
+			  <td>
+				   <a href="#" class="borrar-teclado" data-id="${teclado.id}">X</a>
+			  </td>
+		 `;
+		contenedorCarrito.appendChild(row);
+	});
+
+}
+
+function vaciarCarrito() {
+	// forma rapida (recomendada)
+	while (contenedorCarrito.firstChild) {
+		contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+	}
+}
